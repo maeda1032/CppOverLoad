d684527980c95ae44742876e4a9a07f6cb45073e
maeda1032 <ktc25a31a0053@edu.kyoto-tech.ac.jp>
2026-06-06 13:14:35 +0900

    Vector2クラス

--- CppOverLoad/CppOverLoad/CppOverLoad.cpp
+++ CppOverLoad/CppOverLoad/CppOverLoad.cpp
@@ -1,6 +1,27 @@
 ﻿
 #include <iostream>
 
+class Vector2
+{
+public:
+	Vector2();
+	~Vector2();
+	void operator +(int x);
+	void operator +(int y);
+private:
+
+};
+
+Vector2::Vector2()
+{
+
+}
+
+Vector2::~Vector2()
+{
+
+}
+
 int main()
 {
    
